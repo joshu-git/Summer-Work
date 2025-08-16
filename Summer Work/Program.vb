be597e3a7668dc1212582fd0a2a124c476a3c554
JoshPod5555 <joshuajackargentbusiness@hotmail.com>
2025-08-16 14:22:37 +0100

    Add project files.

---
+++ Summer Work/Program.vb
@@ -0,0 +1,13 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim userselect As Integer = 0
+        Console.ForegroundColor = ConsoleColor.Yellow
+        Console.WriteLine("***** Summer Work *****")
+        Console.WriteLine()
+        Console.WriteLine("Please select one of the following:")
+        Console.WriteLine("(1) ")
+        Console.ReadLine()
+    End Sub
+End Module
