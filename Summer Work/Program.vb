3d332d0d992d4724f37c11cee1781d46055c2012
JoshPod5555 <joshuajackargentbusiness@hotmail.com>
2025-08-16 22:14:07 +0100

    Create error message for RelationalOperators
    Provides task error message to the user

--- Summer Work/Program.vb
+++ Summer Work/Program.vb
@@ -324,7 +324,14 @@ Module ProgrammingTasks
         Call MainMenu(MenuName)
     End Sub
     Sub RelationalOperators()
-
+        Dim userinput As Integer = 1
+        While userinput = 1
+            Console.WriteLine("Error: No tasks found")
+            Console.ReadLine()
+            userinput = ProgramFooter()
+        End While
+        Dim MenuName As String = ""
+        Call MainMenu(MenuName)
     End Sub
     Sub BooleanOperators()
 
