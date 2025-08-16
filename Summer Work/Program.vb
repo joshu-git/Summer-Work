04642908f44bc0f5ba2e43c91b8d0d35f0113742
JoshPod5555 <joshuajackargentbusiness@hotmail.com>
2025-08-16 14:27:11 +0100

    Create selection interface
    Allows user to view different types of programs

--- Summer Work/Program.vb
+++ Summer Work/Program.vb
@@ -4,10 +4,11 @@ Module Program
     Sub Main(args As String())
         Dim userselect As Integer = 0
         Console.ForegroundColor = ConsoleColor.Yellow
-        Console.WriteLine("***** Summer Work *****")
-        Console.WriteLine()
-        Console.WriteLine("Please select one of the following:")
-        Console.WriteLine("(1) ")
+        Console.WriteLine("***** Summer Work *****" & vbCrLf)
+        Console.WriteLine("Please select one of the following:" & vbCrLf)
+        Console.ForegroundColor = ConsoleColor.White
+        Console.WriteLine("(1) Moderation Exercises")
+        Console.WriteLine("(2) Programming Tasks")
         Console.ReadLine()
     End Sub
 End Module
