c1ac3b6bd42836aa51617a723ac45d75ad7a71a7
JoshPod5555 <joshuajackargentbusiness@hotmail.com>
2025-08-17 11:47:21 +0100

    Call MainMenu from StringHandlingFunctions
    Allows user to return to the main menu

--- Summer Work/Program.vb
+++ Summer Work/Program.vb
@@ -477,6 +477,8 @@ Module ProgrammingTasks
             Console.ReadLine()
             userinput = ProgramFooter()
         End While
+        Dim MenuName As String = ""
+        Call MainMenu(MenuName)
     End Sub
     Sub StringConversionFunctions()
 
